915bfa23b9e02a337ccf333bb545c9a6c5f39cbb
Brian Hoole <31298202+brianhoole@users.noreply.github.com>
2018-02-05 22:14:00 -1000

    Attempt 2 9m 34s

--- ziplist.js
+++ ziplist.js
@@ -1,7 +1,7 @@
 function zipList(arrayA, arrayB) {
   const arrayC = [];
   if (arrayA.length !== arrayB.length) {
-    return 'Arrays are different lengths';
+    return 'array lengths don\'t match';
   }
   for (let i = 0; i < arrayA.length; i++) {
     arrayC.push(arrayA[i]);
@@ -10,14 +10,13 @@ function zipList(arrayA, arrayB) {
   return arrayC;
 }
 
-function zipListTheSimpleWay(arrayD, arrayE) {
+console.log(zipList(['a', 'b', 'c'], [1, 2, 3]));
+
+function zipListTheEasyWay(arrayD, arrayE) {
   if (arrayD.length !== arrayE.length) {
-    return 'Arrays are different lengths';
+    return 'arrays don\'t match';
   }
   return _.flatten(_.zip(arrayD, arrayE));
 }
 
-console.log(zipList(['a', 'b', 'c'], [1, 2, 3]));
-console.log(zipList(['d', 'e', 'f'], [4, 5]));
-console.log(zipListTheSimpleWay(['d', 'e', 'f'], [4, 5, 6]));
-console.log(zipListTheSimpleWay(['d', 'e', 'f'], [4, 5]));
+console.log(zipListTheEasyWay(['a', 'b', 'c'], [1, 2, 3]));
